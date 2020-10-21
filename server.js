20f4e7d9dea0f30737e26aa5175cfcdb10efcb68
NaNut <galunyu@gmail.com>
2020-10-21 22:57:57 +0700

    [Added] git , .gitignore

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ dotenv.config({ path: "./config/config.env" })
 
 const app = express()
 
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Sercer running in ${process.env.NODE_ENV} mode on port ${PORT}`))
